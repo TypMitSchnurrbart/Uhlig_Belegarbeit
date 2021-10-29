6229d463e99df47c5f3a7799706b0bc74081958b
EnMolch <heckenberger.t-it19@it.dhbw-ravensburg.de>
2021-10-29 13:56:30 +0200

    Reactus in abgespecktus, lookup im Kürzel

--- web_app/src/App.js
+++ web_app/src/App.js
@@ -1,10 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import SearchBar from './Search'
-import myData from './Gesetze.json'
+import laws from './laws.json'
 
 function App() {
-  console.log(myData);
+  const { search } = window.location;
+  const query = new URLSearchParams(search).get('s');
+
+  const matches = traverse_elements(json_elements(laws), query);
+  const matched_laws = lookup_matches(matches, laws);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -13,5 +18,29 @@ function App() {
     </div>
   );
 }
-    
+
+function json_elements(jsonObj){
+    var keys = Object.keys(jsonObj);
+    return keys
+}
+
+function traverse_elements(keys, query){
+    var acc = [];
+    keys.forEach(function(key){
+        if (key.includes(query)){
+            acc.push(key);
+            console.log(key);
+        }
+});
+    return acc;
+}
+
+function lookup_matches(matches, laws){
+    var acc = []
+    matches.forEach(function(key){
+        acc.push(laws[key]);
+    });
+    return acc;
+}
+
 export default App;
